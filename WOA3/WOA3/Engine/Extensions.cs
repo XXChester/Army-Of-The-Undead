295aab9acf85920128bf8ba3a89a9dde72c0dfa2
mcculbr <mcculbr@8ec5e4ca-6654-e54a-8ea6-c57665c7f23a>
2015-06-20 18:29:50 +0000

    Making the AI walk with some room by the walls on all sides
    git-svn-id: http://gwnp-server/repos/Programming/Team%20Work/Week%20Of%20Awesome%20III@1525 8ec5e4ca-6654-e54a-8ea6-c57665c7f23a

--- WOA3/WOA3/Engine/Extensions.cs
+++ WOA3/WOA3/Engine/Extensions.cs
@@ -8,7 +8,9 @@ using Microsoft.Xna.Framework;
 namespace WOA3.Engine {
 	public static class Extensions {
 		//private static Vector2 OFFSET = new Vector2(Constants.TILE_SIZE / 2f);
-		private static Vector2 OFFSET = new Vector2(0f);
+		private static Vector2 OFFSET = new Vector2(Constants.TILE_SIZE / 3f * 2);
+		//private static Vector2 OFFSET = new Vector2(Constants.TILE_SIZE);
+		//private static Vector2 OFFSET = new Vector2(0f);
 		public static Vector2 toVector2(this Point point) {
 			return Vector2.Add(OFFSET, Constants.TILE_SIZE * new Vector2(point.X, point.Y));
 		}
@@ -22,7 +24,15 @@ namespace WOA3.Engine {
 		}
 		public static Rectangle toRectangle(this Point point) {
 			Vector2 v = point.toVector2();
-			return new Rectangle((int)v.X, (int)v.Y, (int)Constants.TILE_SIZE, (int)Constants.TILE_SIZE);
+			//return new Rectangle((int)v.X, (int)v.Y, (int)Constants.TILE_SIZE, (int)Constants.TILE_SIZE);
+
+			Vector2 start = Vector2.Subtract(v, OFFSET);
+			Vector2 end = Vector2.Add(v, new Vector2(Constants.TILE_SIZE));
+
+			/*float half = Constants.TILE_SIZE / 2f;
+			Vector2 start = Vector2.Subtract(v, new Vector2(half));
+			Vector2 end = Vector2.Add(v, new Vector2(half));*/
+			return new Rectangle((int)start.X, (int)start.Y, (int)end.X, (int)end.Y);
 		}
 
 		public static Vector3 toVector3(this Vector2 vector) {
